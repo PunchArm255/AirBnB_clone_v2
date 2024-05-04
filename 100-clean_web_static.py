1bdc8e08507eceed5ee006be0be1eb7fd416b031
Simo Nassiri <simohamednassiri@gmail.com>
2024-05-03 19:42:42 -0700

    task 100 update

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,42 +1,30 @@
 #!/usr/bin/python3
-"""
-This scripty unalives outdated archives
-"""
+# Fabfile deletes outdated archives
+import os
+from fabric.api import *
 
-from fabric.api import env
-
-env.hosts = ['54.242.98.93', '35.168.3.68']
+env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 
 def do_clean(number=0):
+    """Delete out-of-date archives.
+
+    Args:
+        number (int): The number of archives to keep.
+
+    If number is 0 or 1, keep only newest archive. If
+    number is 2, keep newest and second newest archives,
+    etc.
     """
-    Deletes all useless archives in /version &
-    in /data/web_static/releases
-    """
-    from fabric.api import local, run, env
+    number = 1 if int(number) == 0 else int(number)
 
-    number = int(number)
-    if number == 0:
-        number = 1
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
 
-    # remove local archives
-    if env.host_string == env.hosts[0]:  # execute once
-        archives = local("ls -tr versions/ | tr ' ' '\\n' |\
-                          head -n -{}".format(number), capture=True)
-        if archives != '':
-            for archive in archives.split('\n'):
-                ret = local("rm versions/{}".format(archive))
-                if ret.failed:
-                    return False
-
-    # remove remote archives
-    archives = run("ls -tr --hide=test /data/web_static/releases/ \
-                    | tr ' ' '\\n' | head -n -{}".format(number))
-    if archives != '':
-        for archive in archives.split('\n'):
-            ret = run("rm -rf /data/web_static/releases/\
-                       {}".format(archive.strip('\r')))
-            if ret.failed:
-                return False
-
-    return True
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
