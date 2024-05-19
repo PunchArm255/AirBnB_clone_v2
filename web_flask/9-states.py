a24d17f6ef70f677a501fe50c7b71c60380234e6
PunchArm255 <simohamednassiri@gmail.com>
2024-05-19 16:11:27 -0700

    mod to task 9

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,42 +1,32 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states: HTML page with a list of all State objects.
-    /states/<id>: HTML page displaying the given state with <id>.
 """
+Flask application to list states & cities
+"""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
 
 app = Flask(__name__)
-
-
-@app.route("/states", strict_slashes=False)
-def states():
-    """Displays an HTML page with a list of all States.
-
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("9-states.html", state=states)
-
-
-@app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def close_session(error):
+    """
+    Close session
+    """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.route('/states')
+@app.route('/states/<id>')
+def states_and_cities(id=''):
+    """
+    List states and cities
+    """
+    states = storage.all(State)
+    return render_template('9-states.html', id=id, states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
